ba0cecc542faf3b9be33f7643575f4cb9b68ae00
SergOBW <stretiakov23@gmail.com>
2023-10-15 11:33:16 +0300

    Changes

--- Assets/Drill.cs
+++ Assets/Drill.cs
@@ -1,9 +1,13 @@
 using DG.Tweening;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Drill : MonoBehaviour
 {
     [SerializeField] private Transform drillKnife;
+    [SerializeField] private TMP_Text timeRemaining;
+    [SerializeField] private Slider _slider;
 
     private void Start()
     {
